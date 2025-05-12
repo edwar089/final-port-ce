142edc4ff1b310e3ab15fee87f76e78ee6f643e6
Carly <summerlives396@gmail.com>
2025-05-12 14:43:53 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$(".gallery").slick({ // enables slick library to .gallery div
+		dots: true, // enables pagination buttons
+		fade: true	// enables fade transitions between pictures
+	});
+
+});
